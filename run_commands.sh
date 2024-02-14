d6061b6523020be907312c9d6c7edc839e3a0334
SakariaNdadi <oipapi.ndadi@gmail.com>
2024-02-14 13:16:22 +0200

    run_commands script edit

--- run_commands.sh
+++ run_commands.sh
@@ -4,12 +4,18 @@ pipenv shell
 # Install dependencies
 pipenv install
 
+# Collect Staticfiles
+python manage.py collectstatic
+
+
 # Apply migrations
 python manage.py makemigrations
 python manage.py migrate
 
 # Create superuser
-python manage.py createsuperuser --username admin --email admin@email.com --password admin --noinput
+python manage.py createsuperuser --username admin --email admin@email.com --noinput
+python manage.py changepassword admin admin
+
 
 # Load initial data
 python manage.py loaddata apps/company/fixtures/data.json
